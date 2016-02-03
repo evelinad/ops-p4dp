08cf2d4eea3b927bc06597cc9376ef3ddf73bb39
Parag Bhide <parag.bhide@barefootnetworks.com>
2016-02-03 10:38:10 -0800

    Remove un-required files and old directory structure. Keep only bm subdir and Makefile here
    Change-Id: If49638777753a2ae608923ae3f8b6b9ee54d37a3

--- modules/p4ns_common/utest/_make.mk
+++
@@ -1,8 +0,0 @@
-###############################################################################
-#
-# p4ns_common Unit Test Makefile.
-#
-###############################################################################
-UMODULE := p4ns_common
-UMODULE_SUBDIR := $(dir $(lastword $(MAKEFILE_LIST)))
-include $(BUILDER)/utest.mk
--- modules/p4utils/module/auto/make.mk
+++
@@ -1,9 +0,0 @@
-###############################################################################
-#
-# p4utils Autogeneration
-#
-###############################################################################
-p4utils_AUTO_DEFS := module/auto/p4utils.yml
-p4utils_AUTO_DIRS := module/inc/p4utils module/src
-include $(BUILDER)/auto.mk
-
